5a4ddfce21ead7741fe1a574235bddec14dda7d1
MyaThanHtet <myathanhtet.ucsmwa@gmail.com>
2021-12-24 17:59:59 +0630

    first commit

---
+++ app/src/main/java/com/mth/paypalpay/MainActivity.java
@@ -0,0 +1,121 @@
+package com.mth.paypalpay;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button paymentBtn;
+    private EditText paymentMountEdt;
+    private String paymentAmount;
+    private TextView paymentTv;
+    public static final int PAYPAL_REQUEST_CODE = 1;
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_PRODUCTION)
+            .clientId(PayPalConfig.PAYPAL_CLIENT_ID);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(intent);
+        paymentBtn = findViewById(R.id.payment_btn);
+        paymentMountEdt = findViewById(R.id.palpay_amount_edt);
+        paymentTv=findViewById(R.id.payment_title);
+        paymentBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        getPayment();
+    }
+    private void getPayment() {
+        //Getting the amount from editText
+        paymentAmount = paymentMountEdt.getText().toString();
+
+        //Creating a paypalpayment
+        PayPalPayment payment = new PayPalPayment(new BigDecimal(String.valueOf(paymentAmount)), "USD", "Simplified Coding Fee",
+                PayPalPayment.PAYMENT_INTENT_SALE);
+
+        //Creating Paypal Payment activity intent
+        Intent intent = new Intent(this, PaymentActivity.class);
+
+        //putting the paypal configuration to the intent
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+
+        //Puting paypal payment to the intent
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+
+        //Starting the intent activity for result
+        //the request code will be used on the method onActivityResult
+        startActivityForResult(intent, PAYPAL_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // If the result is from paypal
+        if (requestCode == PAYPAL_REQUEST_CODE) {
+
+            // If the result is OK i.e. user has not canceled the payment
+            if (resultCode == Activity.RESULT_OK) {
+
+                // Getting the payment confirmation
+                PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+
+                // if confirmation is not null
+                if (confirm != null) {
+                    try {
+                        // Getting the payment details
+                        String paymentDetails = confirm.toJSONObject().toString(4);
+                        // on below line we are extracting json response and displaying it in a text view.
+                        JSONObject payObj = new JSONObject(paymentDetails);
+                        String payID = payObj.getJSONObject("response").getString("id");
+                        String state = payObj.getJSONObject("response").getString("state");
+                        paymentTv.setText("Payment " + state + "\n with payment id is " + payID);
+                    } catch (JSONException e) {
+                        // handling json exception on below line
+                        Log.e("Error", "an extremely unlikely failure occurred: ", e);
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                // on below line we are checking the payment status.
+                Log.i("paymentExample", "The user canceled.");
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                // on below line when the invalid paypal config is submitted.
+                Log.i("paymentExample", "An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/mth/paypalpay/PayPalConfig.java
@@ -0,0 +1,8 @@
+package com.mth.paypalpay;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AdhYG32TwJ-2LWRtGLuH0PcB-DMWUrc4PYzIUvjtD2F5BCw_jAyG6ZPANyUGz7MGZh0UbwKzscfZsS03";
+//AbbBxXh6oAaOH1KpEFEMKqGhXalckXmmPTmAoUEQA_1L75WA_a12CtHWPFAsYmIifhsIOBTj8BDOnVhi
+//AV2yRgOXl_JoALOPhZ8VqtYhRzVmVbtvqn8bPHT7BcvxX7a7oi2PMzvUHDfn9aCiI9ac49HNA47dlqGF
+    //AdhYG32TwJ-2LWRtGLuH0PcB-DMWUrc4PYzIUvjtD2F5BCw_jAyG6ZPANyUGz7MGZh0UbwKzscfZsS03
+}
